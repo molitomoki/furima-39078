4081e50c2919f6176d3aaca2bee3390542f87719
molitomoki <molitomoki@gmail.com>
2023-03-18 18:25:11 +0900

    ユーザーの新規登録ができるように実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  validates :last_name,           presence: true
-  validates :first_name,          presence: true
-  validates :last_name_reading,   presence: true
-  validates :first_name_reading,  presence: true
-  validates :birth_day,           presence: true
+  validates :nickname,               presence: true
+  validates :email,                  presence: true
+  validates :password,               presence: true
+  validates :password_confirmation,  presence: true
+  validates :last_name,              presence: true
+  validates :first_name,             presence: true
+  validates :last_name_reading,      presence: true
+  validates :first_name_reading,     presence: true
+  validates :birth_day,              presence: true
 
 end
