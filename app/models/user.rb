e8394cc4dd2b7bd770a053751e6965eca26eed05
molitomoki <molitomoki@gmail.com>
2023-03-20 14:35:14 +0900

    エラーハンドリングの際、重複したエラーメッセージが表示されないように実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,               presence: true
-  validates :email,                  presence: true
-  validates :password,               presence: true
+  #validates :email,                  presence: true
+  #validates :password,               presence: true
   validates :password_confirmation,  presence: true
-  validates :last_name,              presence: true
-  validates :first_name,             presence: true
-  validates :last_name_reading,      presence: true
-  validates :first_name_reading,     presence: true
+  #validates :last_name,              presence: true
+  #validates :first_name,             presence: true
+  #validates :last_name_reading,      presence: true
+  #validates :first_name_reading,     presence: true
   validates :birth_day,              presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
