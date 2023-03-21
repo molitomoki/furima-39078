e499f0eed181e6252e61f5746d64770ad244bf7c
molitomoki <molitomoki@gmail.com>
2023-03-21 17:54:19 +0900

    ActiveHashを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
   def index
+    
+  end
+
+  def new
+    @item = Item.new
   end
 
   #private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,29 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_fee_status
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :scheduled_delivery
+
+  validates :name, :info, :price, presence: true
+
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  
 end
