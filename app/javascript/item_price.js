d7ca1258adea7525d1305d8a061f5ec44a6269f7
molitomoki <molitomoki@gmail.com>
2023-03-23 17:47:33 +0900

    不要なデバッグ用のコードを削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,16 +3,13 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1 );
-    console.log(addTaxDom.innerHTML);
 
     const addProfitDom = document.getElementById("profit");
     const tax = Math.floor(inputValue * 0.1 );
     addProfitDom.innerHTML = inputValue - tax ;
-    console.log(addProfitDom.innerHTML);
 
   })
 });
