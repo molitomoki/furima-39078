e0a63963fcef2acc2bbd7f6e68cc3aead9393270
molitomoki <molitomoki@gmail.com>
2023-03-23 15:50:03 +0900

    不要なコメントの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name,:info,:genre_id,:status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price,:image).merge(user_id: current_user.id)
   end
-
-
-  #private
-
-  #def item_params
-  #  params.require(:item).permit(:image).merge(user_id: current_user.id)
-  #end
 end
