29c8d7f4474400f8f9dcd0591c8c7f81c55b297a
molitomoki <molitomoki@gmail.com>
2023-03-24 18:04:36 +0900

    入力に問題がある時に情報が保持されるように編集
    renderを使う時には、ビューファイルに情報を受け渡す必要があるので、インスタンス変数を定義する必要がある。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,8 +10,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    if item.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -26,8 +26,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
