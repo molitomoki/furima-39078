b0c797de90072ac7c48add8a461cf8483430b2e5
molitomoki <molitomoki@gmail.com>
2023-03-24 17:08:59 +0900

    編集が完了したら、商品詳細表示ページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to root_path
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
     else
       render :edit
     end
