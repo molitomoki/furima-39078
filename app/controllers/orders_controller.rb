5133dd698cb1195b5ea9beccd77d8125d5df42bc
molitomoki <molitomoki@gmail.com>
2023-03-28 20:06:13 +0900

    ユーザーによる条件分岐の実装
    - ログイン状態の場合でも、自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移する
    
    - ログアウト状態の場合は、商品購入ページに遷移しようとすると、商品の販売状況に関わらずログインページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_top
+
   def index
     @order_address = OrderAddress.new
   end
@@ -17,6 +21,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_top
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
 end
