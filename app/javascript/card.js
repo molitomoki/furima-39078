e4f31a434753236d3dfd07d225354fa10dcfac9f
molitomoki <molitomoki@gmail.com>
2023-03-29 19:55:35 +0900

    決済機能のクライアントサイド実装
    クレジットカード情報のトークン化

---
+++ app/javascript/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  const payjp = Payjp('pk_test_a96349b37303700f93180343')
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const submit = document.getElementById("button");
+
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
