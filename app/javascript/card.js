4de726194a0980e6afe1d34a1122b1be3bb88383
molitomoki <molitomoki@gmail.com>
2023-03-29 21:39:30 +0900

    環境変数の設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_a96349b37303700f93180343')
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
