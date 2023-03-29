cb7137b6c48890d3979af6ebfb2d7ee9d38e29e0
molitomoki <molitomoki@gmail.com>
2023-03-29 16:05:10 +0900

    Formオブジェクトパターンでのテストコードの実装

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    city { '足立区' }
+    house_number { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { '09012345678' }
+
+    association :item
+    association :user
+
+  end
+end
