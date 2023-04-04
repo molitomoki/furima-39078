fb5675694af305a4e37716e67278e682e99e529f
molitomoki <molitomoki@gmail.com>
2023-04-04 12:56:22 +0900

    レポジトリ名の修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.2'
 set :application, 'furima-39078'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:molitomoki/furima39078.git'
+set :repo_url,  'git@github.com:molitomoki/furima-39078.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
